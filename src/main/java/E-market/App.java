4d706e388c02cf4c52c5adea26c78ea5ff8c1e2c
uday <udaykumar.aws@gmail.com>
2018-06-20 15:13:38 +0530

    add pom src

---
+++ src/main/java/E-market/App.java
@@ -0,0 +1,17 @@
+package flipkart;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello world!" );
+
+
+        System.out.println( "Hello world!" );
+
+        System.out.println( "Hello java world!" );
+		
+    }
+}
+
